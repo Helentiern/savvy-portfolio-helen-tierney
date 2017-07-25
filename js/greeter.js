db5bcb00066803bb1179ea47334e80a274ef19db
Helen Tierney <helentiern2@gmail.com>
2017-07-24 20:43:27 -0500

    create choose your adventure html and js

--- js/greeter.js
+++ js/greeter.js
@@ -1,6 +1,8 @@
 var userName = prompt( "What's your name?" );
 var titleElement = document.querySelector( "h1" );
+var context = document.querySelector( "p" );
 var titleText = titleElement.textContent;
+var backgroundColor = prompt( "What is your favorite primary color?" );
 
 var checkUserName = function checkUserName(){
     if( !userName ){
@@ -12,5 +14,18 @@ var checkUserName = function checkUserName(){
 
 checkUserName();
 
-
 titleElement.textContent = titleText + ", " + userName;
+
+var setTextBySelector = function setTextBySelector( selector, banana ){
+    var element = document.querySelector( selector );
+
+    if( element ){
+        element.textContent = banana;
+    }
+};
+
+setTextBySelector( "li", "Here it is!" );
+
+var fizzBuzzer = function fizzBuzzer( divisor ){
+
+};
